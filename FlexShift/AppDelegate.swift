90d4b9d300f7dda3f3bd189a0b6c9e8c17cb3e70
Ihounokyaku <kamisama42@gmail.com>
2018-11-30 14:53:38 +0700

    Initial Commit

---
+++ FlexShift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FlexShift
+//
+//  Created by Dylan Southard on 2018/11/30.
+//  Copyright © 2018 Dylan Southard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FlexShift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FlexShift
+//
+//  Created by Dylan Southard on 2018/11/30.
+//  Copyright © 2018 Dylan Southard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
